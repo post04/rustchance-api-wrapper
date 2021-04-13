b21a93556b86ab7a7aa46d845a8f0e1eff0cae93
post <61803796+postrequest69@users.noreply.github.com>
2021-04-12 23:01:09 -0400

    make http requests simpler

--- http.go
+++ http.go
@@ -5,28 +5,63 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strings"
 )
 
-// AccountLeaderboard gets the current accounts leaderboard position in the tickets leaderboard, this requires an authorization token to be set and **WILL** error if one is not provided
-func (s *Session) AccountLeaderboard() (*AccountLeaderboard, error) {
-	if s.Auth == "" {
-		return nil, errors.New("no auth token set")
+// MakeRequest builds a new request, it's to chop down on reused code
+func (s *Session) MakeRequest(auth bool, method, URL string, body *strings.Reader) (*http.Request, error) {
+	if auth {
+		if s.Auth == "" {
+			return nil, errors.New("no auth token set")
+		}
+		req, err := http.NewRequest(method, URL, body)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("cookie", "token="+s.Auth)
+		return req, nil
 	}
-	c := http.Client{}
-	req, err := http.NewRequest("GET", AccountLeaderboardURL, nil)
+	req, err := http.NewRequest(method, URL, body)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("cookie", "token="+s.Auth)
+	return req, nil
+}
+
+// GetBody does all the misc checking and returns the byte body of an http request
+func (s *Session) GetBody(req *http.Request) ([]byte, error) {
+	c := http.Client{}
 	resp, err := c.Do(req)
 	if err != nil {
-		return nil, err
+		return []byte{}, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		return nil, errors.New("http request failed")
+		return []byte{}, errors.New("http request failed with code " + resp.Status)
 	}
 	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return b, nil
+}
+
+// AllInOneHTTP is just a wrap of MakeRequest and GetBody to chop down on reused code while letting me keep control in the future
+func (s *Session) AllInOneHTTP(auth bool, Method, URL string, body *strings.Reader) ([]byte, error) {
+	req, err := s.MakeRequest(auth, Method, URL, body)
+	if err != nil {
+		return []byte{}, err
+	}
+	b, err := s.GetBody(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	return b, nil
+}
+
+// AccountLeaderboard gets the current accounts leaderboard position in the tickets leaderboard, this requires an authorization token to be set and **WILL** error if one is not provided
+func (s *Session) AccountLeaderboard() (*AccountLeaderboard, error) {
+	b, err := s.AllInOneHTTP(true, "GET", AccountLeaderboardURL, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -40,20 +75,7 @@ func (s *Session) AccountLeaderboard() (*AccountLeaderboard, error) {
 
 // GetLeaderboard fetches the data for the current users in the tickets leaderboard, this requires no authorization and therefor doesn't use any cookie headers
 func (s *Session) GetLeaderboard() (*TicketsLeaderboardResult, error) {
-	c := http.Client{}
-	req, err := http.NewRequest("GET", TicketsLeaderboardURL, nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, errors.New("http request failed")
-	}
-	b, err := io.ReadAll(resp.Body)
+	b, err := s.AllInOneHTTP(true, "GET", AccountLeaderboardURL, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -70,24 +92,11 @@ func (s *Session) GetLeaderboard() (*TicketsLeaderboardResult, error) {
 
 // AccountEarnings is the func to get accounts earnings, it returns the amount of money put in and the amount of money won so you can calculate a profit amount. You **need** auth for this func, if auth isn't set it will give back an error
 func (s *Session) AccountEarnings() (*TotalWageredResult, error) {
-	if s.Auth == "" {
-		return nil, errors.New("no auth token set")
-	}
-	c := http.Client{}
-	req, err := http.NewRequest("GET", AccountEarningsURL, nil)
+	req, err := s.MakeRequest(true, "GET", AccountEarningsURL, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("cookie", "token="+s.Auth)
-	resp, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, errors.New("http request failed")
-	}
-	b, err := io.ReadAll(resp.Body)
+	b, err := s.GetBody(req)
 	if err != nil {
 		return nil, err
 	}
